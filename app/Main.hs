ee477b3f82a314f868bfc093712dd94a81bd73f0
vladimir krysanov <vkrysanov320@gmail.com>
2017-08-01 04:11:08 +0300

    Determination of victory function

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Data.Monoid
 
+boardSize :: Int
+boardSize = 3
+
 -- | Итем игрового поля.
 data Item = X | O
   deriving (Eq, Show)
@@ -13,9 +16,12 @@ data Item = X | O
 -- | Игровое поле.
 type Board = [[Maybe Item]]
 
+-- | Победитель
+type Winner = Maybe Item
+
 -- | Инициализация игрового поля (все ячейки пысты -- Nothing).
 initWorld :: Board
-initWorld = replicate 3 (replicate 3 Nothing)
+initWorld = replicate boardSize (replicate boardSize Nothing)
 
 -- | Отрисовка игрового поля.
 renderWorld :: Board -> Picture
@@ -36,20 +42,63 @@ renderWorld board = borders <> items
 
 -- | Хэндлер по нажатию мыши.
 handleWorld :: Event -> Board -> Board
-handleWorld (EventKey (MouseButton LeftButton) Up _ (x, y)) board = insertPoint board X (snap x, snap y)
-  where
-    snap = (+1) . min 1 . max (-1) . fromIntegral . floor . (/ 60) . (+ 30)
-    -- вставка итема на игровое поле по координатам
-    insertPoint board item (x, y) =
-      take x board ++ [take y (board !! x) ++ [Just item] ++ drop (y + 1) (board !! x)] ++ drop (x + 1) board
+handleWorld (EventKey (MouseButton LeftButton) Up _ (x, y)) board =
+  insertPoint board X (snap x, snap y)
+    where
+      snap = (+1) . min 1 . max (-1) . fromIntegral . floor . (/ 60) . (+ 30)
+      -- вставка итема на игровое поле по координатам
+      insertPoint board item (x, y) =
+        take x board ++ [take y (board !! x) ++ [Just item] ++ drop (y + 1) (board !! x)] ++ drop (x + 1) board
 handleWorld _ board = board
 
 -- | Метод, проверяющий победу.
--- gameIsOver :: Board -> Picture
+-- Вычисляется методом сложения X и O; для каждой "полоски" вычисляется значение, которое там установлено, и
+-- записывается в кортеж (a, b), с возможными знчениями 1 или 0, где a -- что поставлен Х, b -- O (мет. itemSum).
+-- После чего  для каждой полоски вычисляется сумма всех значений Х и О соответсвено (так же в кортеже (sX, sO)).
+-- Если sX или sO будет равно 3, то победителем будет, у кого это значение будет равно 3.
+gameIsOver :: Board -> (String, Winner)
+gameIsOver board
+  -- поиск победителя по диагоналям
+  | (fst diagonal  == boardSize) || (snd diagonal  == boardSize) = returnWinner (diagonal , diagonal)  "diag"
+  | (fst diagonal' == boardSize) || (snd diagonal' == boardSize) = returnWinner (diagonal', diagonal') "diag'"
+
+  -- поиск победителя по вертикалям
+  | verticalX || verticalO = returnWinner (verticalX, verticalO) "vert"
+
+  -- поиск по горизонталям
+  | horizontalX || horizontalO = returnWinner (horizontalX, horizontalO ) "hor"
+
+  -- если победа ненайдена
+  | otherwise = ("diag", Nothing)
+  where
+    diagonal  = itemSum $ map (\(i, j) -> board !! i !! j) $ zip [0..boardSize - 1] [0..boardSize - 1]
+    diagonal' = itemSum $ map (\(i, j) -> board !! i !! j) $ zip [0..boardSize - 1] (reverse [0..boardSize - 1])
+
+    -- вычисление значений по вретикали/горизонтали
+    lineVertOrDiag f items =
+      foldr1 (||) $ f $ unzip $ map (\(a, b) -> (a == boardSize, b == boardSize)) items
+
+    verticalX = lineVertOrDiag fst (map itemSum board)
+    verticalO = lineVertOrDiag snd (map itemSum board)
+
+    horizontalX = lineVertOrDiag fst (map itemSum transpon)
+    horizontalO = lineVertOrDiag snd (map itemSum transpon)
+    transpon = foldr1 (zipWith (++)) (map (map (\y -> [y])) board)
+
+    -- сумма итемов
+    itemSum xs = (\(a, b) -> (sum a, sum b)) $ unzip $ map (\x ->
+      case x of
+        (Just X) -> (1, 0)
+        (Just O) -> (0, 1)
+        otherwise -> (0, 0)
+      )xs
+
+    -- вернуть победителя, согласно подсчетам
+    returnWinner (sX, sO) str = if (sX > sO) then (str , Just X) else (str , Just O)
 
 -- | Обновление мира.
 updateWorld :: Float -> Board -> Board
-updateWorld _  board = board
+updateWorld _ board = if ((snd $ gameIsOver board) == Nothing) then board else initWorld
 
 -- | main.
 main :: IO ()
